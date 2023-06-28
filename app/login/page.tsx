64c0730b72e974d087c481432c012b133d8be6d9
Juan Carlos Morales <11338763+jcpr765@users.noreply.github.com>
2023-06-27 22:38:19 -0700

    Maybe now I can actually make that api call?? :prayge:

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,16 @@
-// async function fetchTestData(): Promise<{ message: string }> {
-//   const response = await fetch(process.env.API_URL + "/api/test");
-//   const jsonData = await response.json();
-//   return jsonData;
-// }
+async function fetchTestData(): Promise<{ message: string }> {
+  const response = await fetch(process.env.API_URL + "/api/test");
+  const jsonData = await response.json();
+  return jsonData;
+}
 
 const Login = async () => {
-  // const data = await fetchTestData();
+  const data = await fetchTestData();
 
   return (
     <div>
       <p>This is the login page</p>
-      {/* <p>{data.message}</p> */}
+      <p>{data.message}</p>
     </div>
   );
 };
