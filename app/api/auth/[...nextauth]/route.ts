fc493965ed321203b9953a4ba4fa58d105200f53
Juan Carlos Morales <11338763+jcpr765@users.noreply.github.com>
2023-09-21 22:12:35 -0700

    Remove other cred providers for now

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,32 +5,32 @@ import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   providers: [
-    CredentialsProvider({
-      name: "Email & Password",
-      credentials: {
-        email: { label: "Email", type: "email", placeholder: "your@email.com" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        const res = await fetch("/api/login", {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
-        });
+    // CredentialsProvider({
+    //   name: "Email & Password",
+    //   credentials: {
+    //     email: { label: "Email", type: "email", placeholder: "your@email.com" },
+    //     password: { label: "Password", type: "password" },
+    //   },
+    //   async authorize(credentials, req) {
+    //     const res = await fetch("/api/login", {
+    //       method: "POST",
+    //       body: JSON.stringify(credentials),
+    //       headers: { "Content-Type": "application/json" },
+    //     });
 
-        const user = await res.json();
+    //     const user = await res.json();
 
-        if (res.ok && user) {
-          return user;
-        }
+    //     if (res.ok && user) {
+    //       return user;
+    //     }
 
-        return null;
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
+    //     return null;
+    //   },
+    // }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    // }),
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID ?? "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
