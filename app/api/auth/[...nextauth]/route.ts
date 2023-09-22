72a43f27299778d131e3e050cd5fef19ed7e2f75
Juan Carlos Morales <11338763+jcpr765@users.noreply.github.com>
2023-09-21 23:09:28 -0700

    Setup  prisma adapter for persisting users

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,8 +2,13 @@ import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     // CredentialsProvider({
     //   name: "Email & Password",
